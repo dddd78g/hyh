a5d4ad2ba05141e824b4a4aa37e7e6fa95469cd5
dddd78g <hichymed614@gmail.com>
2025-03-14 17:26:47 +0000

    Update vite.config.ts

--- skills-story-portfolio-main/vite.config.ts
+++ skills-story-portfolio-main/vite.config.ts
@@ -5,18 +5,21 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hyh/", // Set this to match your GitHub repo name
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 }));
